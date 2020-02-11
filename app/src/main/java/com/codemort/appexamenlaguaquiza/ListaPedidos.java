753be6761045462921495c5fc4e84b88222bfbe2
Gilson Chariguaman <gilson258@live.com>
2020-02-10 22:07:05 -0500

    vistas e instancias 2

--- app/src/main/java/com/codemort/appexamenlaguaquiza/ListaPedidos.java
+++ app/src/main/java/com/codemort/appexamenlaguaquiza/ListaPedidos.java
@@ -3,12 +3,29 @@ package com.codemort.appexamenlaguaquiza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListaPedidos extends AppCompatActivity {
+
+    //instancia del elemento de la vista
+    ListView listView;
+    //se crean variables para el conjunto de datos de la lista
+    ArrayList<String> listado;
+
+
+    //para que no colapese la vista
+    protected void onPostResume() {
+        super.onPostResume();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pedidos);
+        listView = (ListView) findViewById(R.id.listView);
+
     }
 }
--- app/src/main/java/com/codemort/appexamenlaguaquiza/Modificar.java
+++ app/src/main/java/com/codemort/appexamenlaguaquiza/Modificar.java
@@ -3,12 +3,49 @@ package com.codemort.appexamenlaguaquiza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Modificar extends AppCompatActivity {
+public class Modificar extends AppCompatActivity implements View.OnClickListener {
+
+    //INSTANCIAR VISTAS
+    TextView txtCodeEdit;
+    EditText txtDetailEdit;
+    EditText txtTotalEdit;
+    EditText txtTypeEdit;
+
+    Button btnEdit;
+    Button btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar);
+
+        txtCodeEdit = (TextView) findViewById(R.id.txtCodeEdit);
+        txtDetailEdit = (EditText) findViewById(R.id.txtDetailEdit);
+        txtTotalEdit = (EditText) findViewById(R.id.txtTotalEdit);
+        txtTypeEdit = (EditText) findViewById(R.id.txtTypeEdit);
+
+        btnEdit = (Button) findViewById(R.id.btnEdit);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+
+        btnEdit.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnEdit:
+               // Toast.makeText(this, "Editar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnDelete:
+                //Toast.makeText(this, "Eliminar", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
