6b91653b80dde64564a007dcbacb73327d1d74be
Gilson Chariguaman <gilson258@live.com>
2020-02-10 23:58:18 -0500

    editar y eliminar

--- app/src/main/java/com/codemort/appexamenlaguaquiza/ListaPedidos.java
+++ app/src/main/java/com/codemort/appexamenlaguaquiza/ListaPedidos.java
@@ -2,11 +2,16 @@ package com.codemort.appexamenlaguaquiza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -29,8 +34,47 @@ public class ListaPedidos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pedidos);
         listView = (ListView) findViewById(R.id.listView);
+
+        //se ejecuta la funcion
         CargarListado();
 
+       // a la lista instanciada se le agrega el evento para dar clic en cualquier item de la lista
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListaPedidos.this, listado.get(position), Toast.LENGTH_SHORT).show();
+                ///se declaran variables para guardar los datos del item
+                String codigo = listado.get(position).split(" ")[0];
+                String detalle = listado.get(position).split(" ")[1];
+                String total = listado.get(position).split(" ")[2];
+                String tipo = listado.get(position).split(" ")[3];
+                Toast.makeText(ListaPedidos.this, "ID: " + codigo, Toast.LENGTH_SHORT).show();
+                /// enviamos parametros paa modificar
+                Intent intent = new Intent(ListaPedidos.this, Modificar.class);
+                intent.putExtra("codigo", codigo);
+                intent.putExtra("detalle", detalle);
+                intent.putExtra("total", total);
+                intent.putExtra("tipo", tipo);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+        ////////7 para poner la flechita de atras
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void CargarListado() {
@@ -47,6 +91,7 @@ public class ListaPedidos extends AppCompatActivity {
         Cursor c = db.rawQuery(sql, null);
         if (c.moveToFirst()) {
             do {
+                //se llena la lista con los datos guardados
                 String linea = c.getString(0) + " " + c.getString(1)+" "+c.getString(2)+" "+c.getString(3);
                 datos.add(linea);
             } while (c.moveToNext());
--- app/src/main/java/com/codemort/appexamenlaguaquiza/Modificar.java
+++ app/src/main/java/com/codemort/appexamenlaguaquiza/Modificar.java
@@ -2,6 +2,10 @@ package com.codemort.appexamenlaguaquiza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +13,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class Modificar extends AppCompatActivity implements View.OnClickListener {
+
+    String codigo;
+    String detalle;
+    String total;
+    String tipo;
 
     //INSTANCIAR VISTAS
     TextView txtCodeEdit;
@@ -20,10 +31,23 @@ public class Modificar extends AppCompatActivity implements View.OnClickListener
     Button btnEdit;
     Button btnDelete;
 
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar);
+
+        //recibe datos que envia la lista
+        Bundle data = getIntent().getExtras();
+        if (data != null) {
+            codigo = data.getString("codigo");
+            detalle = data.getString("detalle");
+            total = data.getString("total");
+            tipo = data.getString("tipo");
+        }
 
         txtCodeEdit = (TextView) findViewById(R.id.txtCodeEdit);
         txtDetailEdit = (EditText) findViewById(R.id.txtDetailEdit);
@@ -33,19 +57,83 @@ public class Modificar extends AppCompatActivity implements View.OnClickListener
         btnEdit = (Button) findViewById(R.id.btnEdit);
         btnDelete = (Button) findViewById(R.id.btnDelete);
 
+        txtCodeEdit.setText(codigo);
+        txtDetailEdit.setText(detalle);
+        txtTotalEdit.setText(total);
+        txtTypeEdit.setText(tipo);
+
         btnEdit.setOnClickListener(this);
         btnDelete.setOnClickListener(this);
+
+        ///////7 para poner la flechita de atras
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        }
     }
 
     @Override
     public void onClick(View v) {
+        String cod = txtCodeEdit.getText().toString();
+        Intent intent = null;
         switch (v.getId()) {
             case R.id.btnEdit:
-               // Toast.makeText(this, "Editar", Toast.LENGTH_SHORT).show();
+                update(cod);
+                // Toast.makeText(this, "Editado", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, ListaPedidos.class);
                 break;
             case R.id.btnDelete:
-                //Toast.makeText(this, "Eliminar", Toast.LENGTH_SHORT).show();
+                destroy(cod);
+                intent = new Intent(this, ListaPedidos.class);
+                // Toast.makeText(this, "Eliminar", Toast.LENGTH_SHORT).show();
                 break;
+        }
+        if(intent != null){
+            startActivity(intent);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            finish();
+        }
+    }
+
+    public void update (String codigo_){
+        Connection db = new Connection(this, "bddemo", null, 1);
+        SQLiteDatabase baseDatos = db.getWritableDatabase();
+        String detalle = txtDetailEdit.getText().toString();
+        String total = txtTotalEdit.getText().toString();
+        String tipo = txtTypeEdit.getText().toString();
+
+
+        if ((!detalle.isEmpty()) && (!total.isEmpty()) && (!tipo.isEmpty()) ) {
+            ContentValues registro = new ContentValues();
+            registro.put("detalle", detalle);
+            registro.put("total", total);
+            registro.put("tipo", tipo);
+
+            baseDatos.update("pedidos", registro, "codigo = '"+codigo_+"'" , null);
+            baseDatos.close();
+
+            Toast.makeText(this, "Se actualizo un pedido", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Hay campos vacios.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public void destroy (String codigo_) {
+        Connection db = new Connection(this, "bddemo", null, 1);
+        SQLiteDatabase baseDatos = db.getWritableDatabase();
+
+        if(!codigo_.equals("")){
+            Cursor fila = baseDatos.rawQuery("SELECT * FROM pedidos WHERE codigo = '"+codigo_+"' ", null);
+            if(fila.getCount() <=0){
+                Toast.makeText(this, "Nada para eliminar.", Toast.LENGTH_SHORT).show();
+            }else {
+                baseDatos.delete("pedidos","codigo = '"+codigo_+"' ",null);
+                baseDatos.close();
+                Toast.makeText(this, "Se elimino.", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Nada para eliminar.", Toast.LENGTH_SHORT).show();
         }
     }
 }
